7e43e268d51121b77fb32186fed2bd49841c6036
sefsa <labrimsn.com@gmail.com>
2026-09-20 12:49:12 +0200

    ping

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+﻿const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  global: true,
+
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Vérifie que le bot est en ligne"),
+
+  async execute(interaction) {
+    await interaction.reply(`🏓 Pong ! Latence : ${interaction.client.ws.ping} ms`);
+  },
+};
--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { Events } = require("discord.js");
+﻿const { Events } = require("discord.js");
 const { GUILD_ID } = require("../config");
 
 module.exports = {
@@ -7,15 +7,16 @@ module.exports = {
   async execute(client) {
     console.log(`✅ Connecté en tant que ${client.user.tag}`);
 
-    const commands = client.commands.map((command) => command.data.toJSON());
+    const globales = client.commands.filter((c) => c.global).map((c) => c.data.toJSON());
+    const locales = client.commands.filter((c) => !c.global).map((c) => c.data.toJSON());
+
+    await client.application.commands.set(globales);
 
     if (GUILD_ID) {
       const guild = await client.guilds.fetch(GUILD_ID);
-      await guild.commands.set(commands);
-    } else {
-      await client.application.commands.set(commands);
+      await guild.commands.set(locales);
     }
 
-    console.log(`📦 ${commands.length} commande(s) enregistrée(s)`);
+    console.log(`📦 ${globales.length} commande(s) globale(s), ${locales.length} commande(s) locale(s)`);
   },
 };
